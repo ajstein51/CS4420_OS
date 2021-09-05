bcce5e6f200afcdd6ec1bf3daa717dae4ba3fe07
AJ Stein <44652091+ajstein51@users.noreply.github.com>
2021-09-05 17:16:17 -0400

    works for most of the input files
    Savepoint

--- project1/testmain.c
+++ project1/testmain.c
@@ -11,16 +11,21 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
-int getsize(int fp);
-int debugvar = 0;
-int firstpass = 0;
+
+// functions:
+int getsize(int linepos);
+int checkmatch(int line1pos, int line2pos);
+
+// Global var
+int debugvar = 0, fp;
+
 int main(int argc, char *argv[])
 {
-    // variable declarations
-    int fp; // open(const char* path, int flag, [, int mode])
 
+    // Open and parse arguments
+//*************************************************************************
     if (argc == 3)
-    {
+    { // open(const char* path, int flag, [, int mode])
         if (strcmp(argv[1], "-d") == 0)
         {
             printf("Debugging mode is on \n");
@@ -29,18 +34,14 @@ int main(int argc, char *argv[])
         fp = open(argv[2], O_RDONLY);
     }
     else if (argc == 2)
-    {
         fp = open(argv[1], O_RDONLY);
-    }
     else
     {
         printf("Too many arguments");
         exit(1);
     }
-
-    // Check if the file can be opened or if its can't be seek'
     if (fp == -1)
-    {
+    {                        // Check if the file can be opened or if its can't be seek'
         perror("Opening: "); // perror(const char *s), gives you a string b4 error msg
         exit(1);
     }
@@ -49,123 +50,240 @@ int main(int argc, char *argv[])
         perror("Cant Seek: ");
         exit(1);
     }
+//*************************************************************************
 
-    int current1 = 0, current2 = 0, linesize1 = 0, linesize2 = 0, check1, check2, nextlinesize;
-    char ch1, ch2;
-    while(1){
-        // getting stuff from 2nd line
-        linesize1 = getsize(fp);
-        if(firstpass == 0){
-            lseek(fp, linesize1, SEEK_SET); // getting to the 2nd line at the start
-            firstpass = 1;
-            current1 = linesize1;
-            nextlinesize = linesize1;
-        }
-        else
-            lseek(fp, current1, SEEK_SET); // keep reading through the sentence
+    // var declarations
+    int size_of_second_line = 0, size_of_first_line = 0, line1pos = 0, line2pos = 0, match = 0;
+    while (1)
+    {
+        // its the first two lines in the file, so we move one line down 
+        if(match == 0){
+            // gets the size of the first line
+            size_of_first_line = getsize(line1pos);
+            //printf("\nThis is size of first: %d \n", size_of_first_line);
 
-        check1 = read(fp, &ch1, 1);
-        current1++; // keep iterating through, so move on to the next char
+            line2pos = size_of_first_line + 1; // puts the 2nd lines pos on the start of the 2nd line
+            //printf("\nThis is line2 %d \n", line2pos);
 
-        // First line
-        //  if(ch1 == '\n'){
-        //     //printf("%d, %d \n", linesize1, linesize2);
-        //     linesize1 = getsize(fp); // get the 3rd lines size
-        //     lseek(fp, current1 - linesize1 + 1, SEEK_SET); // get 2nd line
-        //     linesize2 = getsize(fp); // get size of 2nd line
-        //     if(linesize2 == linesize1){
-        //         //printf("%d\n", linesize2);
-        //         current2 = current1 - linesize1;
-        //     }
-        // }
-        // figure out how to keep the new lines matching each other, so they start on a fresh line when needed
-        
-        // if the 2nd line in the file goes to a new line the first line in the file should move
-        // to a new line aka the next line in the file. Line2 -> line3, line1 -> line2O
-        if(ch1 == '\n'){ 
-            linesize1 = getsize(fp); // gets size of 3rd line
 
-            current2 = current1 - nextlinesize;
-            lseek(fp, current2, SEEK_SET);
+            size_of_second_line = getsize(line2pos); // get size will set the lseek to where ever the argument is 
 
-            linesize2 = getsize(fp); // gets size of 2nd line
+            //printf("\nThis is size of 2nd:  %d\n", size_of_second_line);
 
-            if(linesize2 != linesize1){ // they arent the same size so we move onto the next line
-
+            // if the line sizes match then they are possibly the same, if they are then we check
+            if(size_of_second_line == size_of_first_line){
+                match = checkmatch(line1pos, line2pos);
+                lseek(fp, line1pos, SEEK_SET); // set pos at the line1pos
             }
+            else if(size_of_first_line != size_of_second_line)
+                match = 1;
+            
+        } // end of if
+        if(match == -1){ // the lines match, print out one of them, then move the lines down 1
+            lseek(fp, line1pos, SEEK_SET); // go to 2nd line and print that out
+
+            char tmp;
+            int stopvar = 0, readcheck;
+            while(stopvar == 0){
+                readcheck = read(fp, &tmp, 1);
+
+                // error check
+                if(readcheck == -1){
+                    perror("Readcheck1: ");
+                    exit(1);
+                }
+                else if(readcheck == 0){
+                    if(debugvar == 1)
+                        printf("\nEOF5\n");
+                    exit(0);
+                }
+                if (tmp == '\n')
+                {
+                    stopvar = 1;
+                }
+                write(1, &tmp, 1);
+            }
+            // if we got here then the line is at the \n so we move lines down 1
+            // swap first line info with 2nd line info
+            size_of_first_line = size_of_second_line;
+            line1pos = line2pos;
+
+            // move line1 down another line
+            line1pos += size_of_first_line + 1;
+            size_of_second_line = getsize(line1pos);
+
+
+            // move line2 info down 2 rows
+            line2pos += size_of_second_line + 1;
+            size_of_second_line = getsize(line2pos);
+
+            line2pos += size_of_second_line + 1;
+            size_of_second_line = getsize(line2pos);
+
+            if (size_of_second_line == size_of_first_line)
+            {
+                match = checkmatch(line1pos, line2pos);
+                lseek(fp, line1pos, SEEK_SET); // set pos at the line1pos
+            }
+            if (size_of_second_line != size_of_first_line)
+                match = 1;
+
         }
-        else // keep going through the sentence its at
-            lseek(fp, current2, SEEK_SET);
+        if(match == 1){ // the lines dont match print out first
+            lseek(fp, line1pos, SEEK_SET);
+
+            // print out first line
+            char tmp;
+            int stopvar = 0, readcheck;
+            while (stopvar == 0)
+            {
+                readcheck = read(fp, &tmp, 1);
+
+                // error check
+                if (readcheck == -1)
+                {
+                    perror("Readcheck1: ");
+                    exit(1);
+                }
+                else if (readcheck == 0)
+                {
+                    if (debugvar == 1)
+                        printf("\nEOF6\n");
+                    exit(0);
+                }
+                if (tmp == '\n')
+                {
+                    stopvar = 1;
+                }
+                write(1, &tmp, 1);
+            }
+
+            // move lines first to 3rd, 2nd to 4th
+            size_of_first_line = size_of_second_line;
+            line1pos = line2pos;
+
+            // line1pos += size_of_first_line + 1;
+            // size_of_first_line = getsize(line1pos);
+
+            // other line info
+            line2pos += size_of_second_line + 1;
+            size_of_second_line = getsize(line2pos);
+
+            // line2pos += size_of_second_line + 1;
+            // size_of_second_line = getsize(line2pos);
+
+            if (size_of_second_line == size_of_first_line)
+            {
+                match = checkmatch(line1pos, line2pos);
+                lseek(fp, line1pos, SEEK_SET); // set pos at the line1pos
+            }
+            if (size_of_second_line != size_of_first_line)
+                match = 1;
+        }
         
-        //lseek(fp, current2, SEEK_SET);
-        check2 = read(fp, &ch2, 1);
-
-        // debug
-        printf("%c", ch2);
-        //printf("This is ch1: %c\n", ch1);
-        //printf("This is current1: %d", current1);
-        //printf("This is current2: %d", current2);     
-        //printf("This is ch1: %c, This is ch2: %c \n", ch1, ch2);   
-
-        // if(ch1 == ch2)
-        //     printf("%c", ch1);
-        
-        current2++;
-
-        // checks if something is at the end of file, exit(0) for being exit happy
-        if(check1 == 0 || check2 == 0)
-            exit(0); 
-    } // end of while
+    }     // end of while
+    // shouldnt get down here
     return 0;
+} // end of main
+
+
+// functions
+int getsize(int linepos){
+    char tmp;
+    int count = 0, stopvar = 0;
+    lseek(fp, linepos, SEEK_SET);
+    while(stopvar == 0){
+        read(fp, &tmp, 1); // read through
+        if(tmp == '\n') // its a new line so we stop
+            stopvar = 1;
+        count++;
+    }  // end of loop
+    lseek(fp, linepos, SEEK_SET); // set the position back to what is was
+    return count - 1;
 }
-// when line1 gets to a newline, the 2nd one needs to drop and go to the new line as well. Stop it from lagging behind
-int getsize(int fp){
-    int count = 0;
-    char addy;
-    while(read(fp, &addy, 1) > 0){
-        if(addy != '\n')
-            count++;
-        else if(addy == '\n'){
-            count++;
-            return count;
+
+int checkmatch(int line1pos, int line2pos){
+    char ch1, ch2;
+    int stopvar = 0, readcheck1, readcheck2, lseekcheck1, lseekcheck2, linecounter = 0;
+
+    while(stopvar == 0){
+        lseekcheck1 = lseek(fp, line1pos, SEEK_SET); // seek to first line
+
+        // read the char from the first line
+        readcheck1 = read(fp, &ch1, 1);
+
+        // seek to 2nd line and read
+        lseekcheck2 = lseek(fp, line2pos, SEEK_SET);
+        readcheck2 = read(fp, &ch2, 1);
+
+        if(debugvar == 1)
+            printf("\nThis is Ch1 and Ch2  %c    %c \n", ch1, ch2);
+        
+        // error check
+        if (readcheck1 == -1)
+        {
+            perror("Readcheck1: ");
+            exit(1);
         }
+        else if (readcheck1 == 0)
+        {
+            if (debugvar == 1)
+                printf("\nEOF1\n");
+        }
+        if (readcheck2 == -1)
+        {
+            perror("Readcheck1: ");
+            exit(1);
+        }
+        else if (readcheck2 == 0)
+        {
+            if (debugvar == 1)
+                printf("\nEOF2\n");
+        }
+        if (lseekcheck1 == -1)
+        {
+            perror("Readcheck1: ");
+            exit(1);
+        }
+        else if (lseekcheck1 == 0)
+        {
+            if (debugvar == 1)
+                printf("\nEOF3\n");
+        }
+        if (lseekcheck2 == -1)
+        {
+            perror("Readcheck1: ");
+            exit(1);
+        }
+        else if (lseekcheck2 == 0)
+        {
+            if (debugvar == 1)
+                printf("\nEOF4\n");
+        }
+
+        if(ch1 == '\n'){
+            return -1; // they are the same, print out 1 line only
+        }
+        else if(ch1 != ch2){
+            return 1; // they arent the same, print first line and move lines down
+        }
+
+        line1pos++, line2pos++; // increment
     }
-    
+    if(debugvar == 1)
+        printf("\nWE GOT DOWN HERE\n");
 }
 
 /*
-char addy1, addy2;
-    int len1 = 0, newline = 0, pos1 = 0, pos2 = 0, pos3 = 0;
-    while(1){
-        len1 = read(fp, &addy1, 1);                                          // read(int fd, *buff, count)
-        if(len1 == -1){                                                      // error in reading
-            perror("Reading Error: ");
-            exit(1);
-        }
-        if(len1 == 0){                                                       // leaving the while loop
-            if (close(fp) == -1){                                           // int close(int fd), fd = file descriptor
-                perror("Closing: ");                                        
-                exit(1);
-            }
-            printf("\n");
-            exit(0);                                                        // EOF, empty happy 
-        } 
+Rules
+if they arent the same print the line out
+if they are the same print one of the lines and move down
+if they arent the same size its unique
+if they are the same size check if they are the same
 
-        pos1++;
-        if (newline == 1){
-            lseek(fp, pos2, SEEK_SET);
-            read(fp,&addy2, 1);                                             // dont need to check for -1 cause its reading whats already been read
-            if(debugvar == 1){
-                printf("This is addy1 %c , this is addy2 %c, this is pos1 %d, this is pos2 %d \n", addy1, addy2, pos1, pos2);
+To Do:
+- error checks for lseek, read, write
+- Debug info
+- if one of the lines is 0 then print the other to fix EOF issue?
+*/
 
-            }
-            if(addy2 == addy1 && debugvar == 0){
-                write(1, &addy1, 1);
-            }
-            pos2++;
-            lseek(fp, pos1, SEEK_SET);
-        } // end of if(newline)
-        if(addy1 == '\n'){
-            newline = 1;
-        }
-        */
